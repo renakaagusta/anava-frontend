820d9f2799cdfaff64fd48473545033901a9e40b
renakaagusta <renakaagusta28@gmail.com>
2020-12-05 22:27:23 +0700

    init to deploy

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import header from './header';
 
-const API_URL = 'http://localhost:3000/api/payment/';
+const API_URL = 'http://anavaugm.com:3000/api/payment/';
 
 class PaymentService {
   getAllPayment() {
