69153f3845d02048b26a2e14544c08fa331a2186
renakaagusta <renakaagusta28@gmail.com>
2021-01-09 15:42:25 +0700

    change ip

--- src/services/answer.service.js
+++ src/services/answer.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import header from "./header";
 
-const API_URL = "http://anavaugm.com/api/answer/";
+const API_URL = "http://52.163.218.138:3000/api/answer/";
 
 class AnswerFormService {
   uploadAnswer(answer) {
--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import header from './header';
 
-const API_URL = 'http://anavaugm.com/api/payment/';
+const API_URL = 'http://52.163.218.138:3000/api/payment/';
 
 class PaymentService {
   getAllPayment() {
