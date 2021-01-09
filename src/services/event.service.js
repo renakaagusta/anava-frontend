0cabe5d9faeb4f85a6c05e688a372a427b79715f
renakaagusta <renakaagusta28@gmail.com>
2021-01-09 13:11:22 +0700

    change to anavugm.com

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import header from './header';
 
-const API_URL = 'http://52.163.218.138:3000/api/event/';
+const API_URL = 'http://anavaugm.com:3000/api/event/';
 
 class EventService {
   getAllEvent() {
