5746588d10dd947d6e8ea651435017a8fa04ecd0
renakaagusta <renakaagusta28@gmail.com>
2021-01-16 19:07:44 +0700

    add document

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -182,13 +182,13 @@
                 <img src="/patnership/sponsor/bess-finance.png"  style="height:80px;"/>
               </b-col>
               <b-col lg="3" sm="12">
-                <img src="/patnership/sponsor/alumni-stat.jpg"  style="height:80px;"/>
+                <img src="/patnership/sponsor/alumni-stat.jpg"  style="height:120px; width:120px"/>
               </b-col>
               <b-col lg="3" sm="12">
                 <img src="/patnership/sponsor/im-coco.png"  style="height:80px;"/>
               </b-col>
               <b-col lg="3" sm="12">
-                <img src="/patnership/sponsor/cicil.png"  style="height:80px;"/>
+                <img src="/patnership/sponsor/cicil.png"  style="height:70px;"/>
               </b-col>
             </b-row><br/><br/>
             <h3 class="mt-5">Media Partner :</h3>
